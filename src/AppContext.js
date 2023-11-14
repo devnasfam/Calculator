51a2368f771bd1269cdf4bbc8aaa51b941a4e803
Dev Nasfam <amajidahmad848@gmail.com>
2023-11-14 17:56:31 +0100

    Update AppContext.js

--- src/AppContext.js
+++ src/AppContext.js
@@ -2,11 +2,11 @@ import React, { useState, createContext } from 'react';
 
 export const AppContext = createContext();
 export const AppProvider = ({children}) =>{
-const [value,setValue] = useState("0");
- const [equal,setEqual] = useState();
- const [isDark,setIsdark] = useState(true);
- const [Icon,setIcon] = useState("dark_mode");
- const [isBg,setIsBg] = useState(true);
+const [value, setValue] = useState("0");
+ const [equal, setEqual] = useState();
+ const [isDark, setIsdark] = useState(true);
+ const [Icon, setIcon] = useState("dark_mode");
+ const [isBg, setIsBg] = useState(true);
  const Values = {
      value,setValue,
      equal,setEqual,
