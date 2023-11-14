360d97e642fecdce6ba0eb4904f9e3a81f2ed4c7
Dev Nasfam <amajidahmad848@gmail.com>
2023-11-14 17:55:23 +0100

    Create AppContext.js

---
+++ src/AppContext.js
@@ -0,0 +1,22 @@
+import React, { useState, createContext } from 'react';
+
+export const AppContext = createContext();
+export const AppProvider = ({children}) =>{
+const [value,setValue] = useState("0");
+ const [equal,setEqual] = useState();
+ const [isDark,setIsdark] = useState(true);
+ const [Icon,setIcon] = useState("dark_mode");
+ const [isBg,setIsBg] = useState(true);
+ const Values = {
+     value,setValue,
+     equal,setEqual,
+     isDark,setIsdark,
+     Icon,setIcon,
+     isBg,setIsBg
+ }
+    return(
+       <AppContext.Provider value={Values}>
+        {children}
+       </AppContext.Provider>
+    );
+}
